2e82bd8a958d12826979cfd91ba91dcad5387302
gjani <gatsjanos@gmail.com>
2024-02-05 00:29:08 +0100

    Added CSP headers

--- next.config.js
+++ next.config.js
@@ -2,6 +2,19 @@
 const nextConfig = {
     reactStrictMode: true,
     output: "standalone",
+    headers: () => [{
+        source: '/(.*)',
+        headers: [
+            {
+                key: 'Content-Security-Policy',
+                value: "frame-ancestors 'none';",
+            },
+            {
+                key: 'X-Frame-Options',
+                value: "DENY",
+            },
+        ],
+    }],
 }
 
 module.exports = nextConfig
