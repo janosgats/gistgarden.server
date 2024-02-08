c97b3b97b2fb25d901d7271041771bcf4ca35597
gjani <gatsjanos@gmail.com>
2024-02-09 00:49:53 +0100

    small cleanup

--- src/app/health/ping/route.ts
+++ src/app/health/ping/route.ts
@@ -1,18 +1,7 @@
-import {NextRequest, NextResponse} from "next/server";
-import globalMagicRouter from "@/magicRouter/globalMagicRouter";
-import {withFilters} from "@/apiFilters/withFilters";
+import {NextResponse} from "next/server";
 
 
 export const revalidate = 0
-
-
-async function unfilteredApiHandler(req: NextRequest): Promise<NextResponse> {
-    return await globalMagicRouter.handleRequest(req)
-}
-
-
-const apiHandler = withFilters(unfilteredApiHandler)
-
 
 export const GET = async (): Promise<NextResponse> => {
     return NextResponse.json({message: "UP"});
