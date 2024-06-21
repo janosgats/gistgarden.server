8717624a5efdf6c3e336f2648ccf39bebde407ab
gjani <gatsjanos@gmail.com>
2024-06-21 20:15:56 +0200

    Added a very basic landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,29 @@
 
 import React from "react";
 import Link from "next/link";
-import {Button, Stack, Typography} from '@mui/material';
+import {Button, Stack, Typography, useTheme} from '@mui/material';
+import GrassIcon from '@mui/icons-material/Grass';
 
 
 export default function Home() {
+    const theme = useTheme()
 
     return (
         <Stack alignItems="center" height="100vh" justifyContent="center" spacing={3}>
 
-            <Typography variant="h2" textAlign="center">This will be a landing page</Typography>
+            <Link href="/dashboard" style={{textDecoration: 'none', color: 'inherit'}}>
+                <GrassIcon sx={{fontSize: "13rem", transform: 'translateY(7%)', color: 'lime'}}/>
+            </Link>
+            <Typography variant="h4" textAlign="center" style={{marginTop: theme.spacing(0)}}>
+                Welcome to
+            </Typography>
+            <Typography variant="h2" textAlign="center" style={{marginTop: theme.spacing(2)}}>
+                Gist&nbsp;<span style={{color: "lime"}}>Garden</span>
+            </Typography>
+
 
             <Link href="/dashboard" style={{textDecoration: 'none', color: 'inherit'}}>
-                <Button variant="contained">Go to Dashboard</Button>
+                <Button variant="contained" size="large">Go to Dashboard</Button>
             </Link>
 
         </Stack>
